40a951f02c3362f3788798a6d90cfa59bd187f27
Brenda Carolina Arras <bca240000@utdallas.edu>
2025-04-21 10:56:48 -0500

    Added Parameters

--- src/project.py
+++ src/project.py
@@ -3,3 +3,9 @@ import sounddevice as sd
 from PIL import Image, ImageDraw
 import time
 import os
+
+# Parameters
+WIDTH, HEIGHT = 256, 256
+FPS = 15
+PIXEL_SIZE = 16
+PALETTE = [(0, 0, 0), (255, 0, 0), (255, 255, 0), (0, 255, 0), (0, 255, 255), (0, 0, 255), (255, 0, 255)]
